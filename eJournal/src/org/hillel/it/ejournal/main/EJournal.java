3b024672ae0f36cccf2f8804623a14de99e1a2e9
ithillel-dendec <denias@ukr.net>
2013-04-03 02:52:03 +0300

    Changes in application startup

--- eJournal/src/org/hillel/it/ejournal/main/EJournal.java
+++ eJournal/src/org/hillel/it/ejournal/main/EJournal.java
@@ -1,10 +1,6 @@
 package org.hillel.it.ejournal.main;
 
-import org.hillel.it.ejournal.db.DBHelper;
-import org.hillel.it.ejournal.model.entity.Role;
-import org.hillel.it.ejournal.model.entity.User;
-
-import java.util.GregorianCalendar;
+import org.hillel.it.ejournal.service.io.Session;
 
 public class EJournal {
 
@@ -13,10 +9,7 @@ public class EJournal {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GregorianCalendar cal = new GregorianCalendar(1990, 01, 01);
-		int userid = DBHelper.getInstance().addUser(Role.STUDENT, "name", "surname", cal, "login", "pass");
-		User usr = DBHelper.getInstance().getUserById(userid);
-		System.out.println(usr.getName());
+		new Session();
 		}
 
 }
