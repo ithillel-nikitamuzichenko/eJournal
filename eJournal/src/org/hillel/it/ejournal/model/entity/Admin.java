2d389aebe4c3bff07edbfad5cfb5ca1fb9f8e7f6
ithillel-dendec <denias@ukr.net>
2013-04-03 02:53:57 +0300

    Changes in admin entity

--- eJournal/src/org/hillel/it/ejournal/model/entity/Admin.java
+++ eJournal/src/org/hillel/it/ejournal/model/entity/Admin.java
@@ -2,41 +2,11 @@ package org.hillel.it.ejournal.model.entity;
 
 import java.util.Date;
 
-public class Admin extends Teacher {
+public class Admin extends User {
 
-	public Admin(String name, String surname, Date birthDate, Sex sex) {
-		super(name, surname, birthDate, sex);
+	public Admin(String name, String surname, Date birthDate, Sex sex, String login, String password) {
+		super(name, surname, birthDate, sex, Role.ADMIN, login, password);
 		// TODO Auto-generated constructor stub
-	}
-
-	public int addSchoolClass(String name, int year) {
-		return 0;
-	}
-
-	public int addSubject(String name, double rang) {
-		return 0;
-	}
-
-	public int addStudent(String name, String surname, Date birthDate, Sex sex,
-			int schoolClassId, int groupId) {
-		return 0;
-	}
-
-	public int addTeacher(String name, String surname, Date birthDate, Sex sex) {
-		return 0;
-	}
-
-	public void addTeacherSubjectLink(int teacherId, int subjectId) {
-
-	}
-
-	public void addTeacherClassLink(int teacherId, int schoolClassId) {
-
-	}
-
-	public int addLesson(int subjectId, int schoolClassid, int groupId,
-			int dayNumber, int lessonNumber) {
-		return 0;
 	}
 
 }
