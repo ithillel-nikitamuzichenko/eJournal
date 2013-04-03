c42f53e20a47fa05fcd35bcc64eb69d485ff624e
ithillel-dendec <denias@ukr.net>
2013-04-03 12:44:42 +0300

    Changes in database

--- eJournal/db/Journal.sql
+++ eJournal/db/Journal.sql
@@ -6,8 +6,9 @@ CREATE TABLE IF NOT EXISTS `EJournal`.`Users` (
   `Name` VARCHAR(45) NULL ,
   `Surname` VARCHAR(45) NULL ,
   `BirthDate` DATETIME NULL ,
+  `Sex` INT NULL,
   `Login` VARCHAR(45) NOT NULL ,
   `Password` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`ID`) ,
   UNIQUE INDEX `ID_UNIQUE` (`ID` ASC));
-INSERT INTO `EJournal`.`Users` VALUES (null, 2, null, null, null, 'Admin', '12345');
+INSERT INTO `EJournal`.`Users` VALUES (null, 2, null, null, null, 1, 'admin', '12345');
