c46f3237c5084528561c5ab1dbc2ab225835faa4
ithillel-dendec <denias@ukr.net>
2013-04-09 21:02:33 +0300

    Utility class, that provides input prompt with checking of input values

---
+++ eJournal/src/org/hillel/it/ejournal/service/io/Asker.java
@@ -0,0 +1,72 @@
+package org.hillel.it.ejournal.service.io;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class Asker {
+	public static String askString(String message, boolean isObligatory,
+			Scanner scanner) {
+		String string;
+		do {
+			System.out.print(message);
+			string = scanner.nextLine();
+		} while ((string.isEmpty()) && isObligatory);
+		return string;
+	}
+
+	public static Integer askInteger(String message, boolean isObligatory,
+			int minVal, int maxVal, Scanner scanner) {
+		Integer value = null;
+		boolean isCorrect = false;
+		do {
+			value = null;
+			System.out.print(message);
+			try {
+				value = Integer.valueOf(scanner.nextLine());
+			} catch (NumberFormatException e) {
+				System.out.println("Ivalid number");
+			}
+			if ((value != null) && ((value < minVal) || (value > maxVal))) {
+				System.out
+						.println(String.format(
+								"Value must be in range from %d to %d", minVal,
+								maxVal));
+			} else {
+				isCorrect = true;
+			}
+		} while (((value == null) && isObligatory) || !isCorrect);
+		return value;
+	}
+
+	public static Date askDate(String message, SimpleDateFormat sdf,
+			boolean isObligatory, Scanner scanner) {
+		Date date = null;
+		boolean isCorrect = false;
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.add(Calendar.YEAR, -90);
+		do {
+			date = null;
+			try {
+				System.out.print(message);
+				date = sdf.parse(scanner.nextLine());
+			} catch (ParseException e) {
+				System.out.println("Ошибка ввода даты");
+			}
+			if ((date != null)
+					&& ((date.before(calendar.getTime())) || (date
+							.after(Calendar.getInstance().getTime())))) {
+				System.out.println(String.format(
+						"Value must be in range from %s to %s",
+						sdf.format(calendar.getTime()),
+						sdf.format(Calendar.getInstance().getTime())));
+			} else {
+				isCorrect = true;
+			}
+		} while (((date == null) && isObligatory) || !isCorrect);
+		return date;
+	}
+}
