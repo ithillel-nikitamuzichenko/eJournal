932ea413b4f2ac67d7c4c8bf512e9f78cf9856e2
ithillel-dendec <denias@ukr.net>
2013-04-10 16:03:08 +0300

    added method that returns number of students.

--- eJournal/src/org/hillel/it/ejournal/model/entity/SchoolClass.java
+++ eJournal/src/org/hillel/it/ejournal/model/entity/SchoolClass.java
@@ -1,15 +1,20 @@
 package org.hillel.it.ejournal.model.entity;
 
+import org.hillel.it.ejournal.service.io.Service;
+
 public class SchoolClass implements Entity {
 	private String name;
 	private int year;
+	private Service service;
 	protected Integer id;
+	
 
-	public SchoolClass(String name, int year) {
+	public SchoolClass(String name, int year, Service service) {
 		this.name = name;
 		this.year = year;
+		this.service = service;
 	}
-	
+
 	@Override
 	public EntityType getEntityType() {
 		return EntityType.SCHOOL_CLASS;
@@ -31,18 +36,26 @@ public class SchoolClass implements Entity {
 	public EntityLog getEntityLog() {
 		return null;
 	}
+
+	public static String stringHeader() {
+		return String.format("%4s%5s%7s%9s", "id", "Year", "Letter", "Students");
+	}
 	
 	@Override
 	public String toString() {
-		return String.format("%d%s", year, name.toUpperCase());
+		return String.format("%4d%5d%7s%9d", id, year, name.toUpperCase(), getStudentsNumber());
 	}
-	
+
 	public int getYear() {
 		return year;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public int getStudentsNumber() {
+		return service.getClassList(id).size();
 	}
 
 }
